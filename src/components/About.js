dd507c63455cdeff81994445e3440da3f3a13d2f
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-11-28 09:24:57 -0500

    final product

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+  const isBio = props.bio;
+  if (!isBio) {
+    return null
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/" />
     </div>
   );
 }
 
 export default About;
+
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
@@ -10,9 +11,9 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home color="firebrick" name="Liza" city="New York"/>
+      <About {...user}/>
     </div>
   );
 }
